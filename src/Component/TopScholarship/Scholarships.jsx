4eb8d16bfaa59848bd05069c2a3c9bbfea73f2d6
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-06-07 00:28:01 +0600

    get scholarships data from server

--- src/Component/TopScholarship/Scholarships.jsx
+++ src/Component/TopScholarship/Scholarships.jsx
@@ -2,25 +2,27 @@ import { useEffect, useState } from "react";
 import SectionTitle from "../SectionTitle/SectionTitle";
 import { Link } from "react-router-dom";
 import Scholarship from "../Scholarship/Scholarship";
+import { axiosPublic } from "../../Hooks/useAxiosPublic";
 
 const Scholarships = () => {
   const [scholarships, setScholarships] = useState([]);
+
   useEffect(() => {
-    fetch("scholarship.json")
-      .then((res) => res.json())
-      .then((data) => setScholarships(data));
+    axiosPublic.get("/scholarships").then((res) => {
+      setScholarships(res.data);
+    });
   }, []);
 
-  const sortedScholarships = scholarships.sort((a, b)=>{
-    const firstItem = parseInt(a.applicationFees.split('$')[1])
-    const secondItem = parseInt(b.applicationFees.split('$')[1])
-    
+  const sortedScholarships = scholarships.sort((a, b) => {
+    const firstItem = parseInt(a.applicationFees.split("$")[1]);
+    const secondItem = parseInt(b.applicationFees.split("$")[1]);
+
     if (firstItem === secondItem) {
       return new Date(b.postDate) - new Date(a.postDate);
     }
 
     return firstItem - secondItem;
-  })
+  });
 
   return (
     <div className="container mx-auto px-3 lg:px-12 my-8 lg:my-14">
@@ -39,7 +41,9 @@ const Scholarships = () => {
         ))}
       </div>
       <Link className="flex justify-center my-6">
-        <button className="btn bg-orange-400 text-white">All Scholarships</button>
+        <button className="btn bg-orange-400 text-white">
+          All Scholarships
+        </button>
       </Link>
     </div>
   );
