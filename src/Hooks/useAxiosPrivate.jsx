c35dba40d88d6c127766d80e0ed3a4bae0300835
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-06-08 02:58:14 +0600

    axiosSecure hook with interceptor

---
+++ src/Hooks/useAxiosPrivate.jsx
@@ -0,0 +1,44 @@
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
+
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+});
+const useAxiosPrivate = () => {
+  const navigate = useNavigate();
+  const { logoutUser } = useAuth();
+
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      const token = localStorage.getItem("token");
+
+      config.headers.authorization = `Bearer ${token}`;
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+      console.log("status error in the interceptor", status);
+
+      if (status === 401 || status === 403) {
+        await logoutUser();
+        navigate("/login");
+      }
+
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosSecure;
+};
+
+export default useAxiosPrivate;
