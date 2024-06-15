1dae5e7162c91af6595182032cb9c028335a79d5
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-06-16 03:22:05 +0600

    review card design and handle delete card

--- src/Component/Review/Review.jsx
+++ src/Component/Review/Review.jsx
@@ -1,27 +1,77 @@
+import { Rating } from "@smastrom/react-rating";
 import PropTypes from "prop-types";
+import Swal from "sweetalert2";
+import useAxiosPrivate from "../../Hooks/useAxiosPrivate";
 
-const Review = ({review}) => {
+const Review = ({ review, refetch }) => {
+  const axiosSecure = useAxiosPrivate();
+
+  const handleReviewDelete = (reviewId) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await axiosSecure.delete(`/reviews/${reviewId}`);
+
+        if (res.data.deletedCount === 1) {
+          Swal.fire({
+            title: "Deleted!",
+            text: "Review has been deleted.",
+            icon: "success",
+          });
+          refetch();
+        }
+      }
+    });
+  };
   return (
-      <div className="card card-side bg-base-100 shadow-xl">
-        <figure>
-          <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.jpg"
-            alt="Movie"
-          />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">New movie is released!</h2>
-          <p>Click the button to watch on Jetflix app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Watch</button>
+    <div className="card items-center bg-base-100 shadow-xl">
+      <div className="card-body">
+        <div className="flex gap-3 mb-2">
+          <div className="avatar">
+            <div className="w-16 rounded-full">
+              <img src={review.reviewerImage} />
+            </div>
+          </div>
+
+          <div>
+            <h2 className="card-title">{review.reviewerName}</h2>
+            <h2 className="text-gray-500">
+              <span>Posted On:</span> {review.reviewDate}
+            </h2>
           </div>
         </div>
+
+        <p>{review.reviewerComments}</p>
+        <div className="card-actions justify-center my-2">
+          <Rating
+            style={{ maxWidth: 180 }}
+            value={review.ratingPoint}
+            readOnly
+          />
+        </div>
+        <div className="card-actions justify-end">
+          <button
+            onClick={() => handleReviewDelete(review._id)}
+            className="btn bg-orange-400 text-white"
+          >
+            Delete
+          </button>
+        </div>
       </div>
+    </div>
   );
 };
 
 Review.propTypes = {
-    review: PropTypes.object,
+  review: PropTypes.object,
+  refetch: PropTypes.func,
 };
 
 export default Review;
--- src/Pages/Dashboard/Moderator/AllReviews.jsx
+++ src/Pages/Dashboard/Moderator/AllReviews.jsx
@@ -3,13 +3,13 @@ import SectionTitle from "../../../Component/SectionTitle/SectionTitle";
 import useReviews from "../../../Hooks/useReviews";
 
 const AllReviews = () => {
-    const [reviews] = useReviews();
+    const [reviews, refetch] = useReviews();
     return (
         <div>
             <SectionTitle title={"All Reviews"}></SectionTitle>
             <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 container mx-auto px-2 lg:px-8">
                 {
-                    reviews.map(review => <Review key={review._id} review={review}></Review>)
+                    reviews.map(review => <Review key={review._id} review={review} refetch={refetch}></Review>)
                 }
             </div>
         </div>
